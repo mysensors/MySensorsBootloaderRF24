95368dfc8a74881793839b574e40d050d88b4f1d
Ziehe Enrico <E.Ziehe@strube.net>
2018-09-20 18:49:55 +0200

    Add Config Option for CE7 and CSN10 (#31)

--- MYSBootloader.c
+++ MYSBootloader.c
@@ -64,13 +64,14 @@
 #define BOOTLOADER_COMMANDS					// Enable low level bootloader commands
 
 // SPI bus setting *****************************************************************************************************
-#define SPI_PINS_CE9_CSN10							
+//#define SPI_PINS_CE9_CSN10							
 //define SPI_PINS_CSN7_CE8
+#define SPI_PINS_CE7_CSN10
 
 // LED settings ********************************************************************************************************
 #define LED_DDR     DDRB
 #define LED_PORT    PORTB
-#define LED_PIN		PINB5
+#define LED_PIN		PB1
 
 // DEBUG settings ******************************************************************************************************
 //#define DEBUG
