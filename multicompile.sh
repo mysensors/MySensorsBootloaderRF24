d0cd38752532fd51fa3ed62ee0714193b0177214
John de Rooij <john.de.rooij@gmail.com>
2020-12-06 14:30:01 +0100

    Log info of the used compiler to the compiled directory

--- multicompile.sh
+++ multicompile.sh
@@ -3,6 +3,22 @@ power_levels=( RF24_PA_MIN RF24_PA_LOW RF24_PA_HIGH RF24_PA_MAX )
 
 mkdir compiled
 
+compiler=compiled/compiler-info.txt
+echo -e "avr-gcc version:\n================" > ${compiler}
+# avr-gcc seems to dump its specfications on stderr, so we have to redirect stderr
+avr-gcc -### 2>> ${compiler}
+echo -e "\navr-ld version:\n===============" >> ${compiler}
+avr-ld --version >> ${compiler}
+echo -e "\navr-objcopy version:\n====================" >> ${compiler}
+avr-objcopy -V >> ${compiler}
+echo -e "\navr-size version:\n=================" >> ${compiler}
+avr-size --version >> ${compiler}
+echo -e "\nCFLAGS:\n=======" >> ${compiler}
+grep "^CFLAGS" >> ${compiler} Makefile
+echo -e "\nLDFLAGS:\n========" >> ${compiler}
+grep "^LDFLAGS" >> ${compiler} Makefile
+
+
 for f in "${clock_speeds[@]}"
  do
  for i in {1..110}
